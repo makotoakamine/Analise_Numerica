b1960db7e2b2d4f9a095627e812b228a3da17b3b
makotoakamine <makotoakamine12@gmail.com>
2017-10-18 05:05:53 +0000

    Adicionado Jacobi otimizado

---
+++ Jacobi_otimizado.c
@@ -0,0 +1,57 @@
+//Modificar o trecho da multiplicacao
+//Otimizar levando em conta que a matriz diagonal nao precisa
+//ser invertida.
+//Fazer uma matriz esparsa para calcular os termo dependentes.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "BasicFunc.h"
+
+void main(){
+    float **M;
+    float *V;
+    float *MV_temp;
+    float *VV;
+    float **X;
+    float *D;
+    float **R;
+    int i = 0;
+    int j = 1;
+    int n;
+    
+    
+    double acc=0;
+    clock_t t1, t2;
+    //Inicializacao da Matriz
+    scanf("%d",&n);
+    M = allocM(n);
+    fillM(M,n);
+    V = allocV(n);
+    fillV(V,n);
+    MV_temp = allocV(n);
+    VV = allocV(n);
+    X = (float **)malloc(2*sizeof(float *)); 
+    X[0] = allocV(n);
+    X[1] = allocV(n);
+    zeroFyV(X[0],n);
+    zeroFyV(X[1],n);
+    D = allocV(n);
+    R = allocM(n);
+    sparse_stripDR(M,D,R,n);
+    t1 = clock();
+    //trecho a ser cronometrado
+    for(i=0;i<100;i++){
+        MV_temp = scalarMultMV(MV_temp,R,X[i&1],n); 
+        VV = subVV(VV,V,MV_temp,n); 
+        for(j=0;j<n;j++){
+            X[(i+1)&1][j] = VV[j]/D[j];
+        }
+    }
+    
+    
+    t2 = clock();
+    acc += (double)(t2-t1)/CLOCKS_PER_SEC;
+    printf("Tempo(s): %.6f\n", (double)(t2-t1)/CLOCKS_PER_SEC);
+    printV(X[i&1],n);
+    printf("\n");
+}

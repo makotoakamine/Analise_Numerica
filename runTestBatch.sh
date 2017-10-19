f0115c041c3a184f87187b000802ac360f2247fc
makotoakamine <makotoakamine12@gmail.com>
2017-10-19 14:04:21 +0000

    Finalizado

---
+++ runTestBatch.sh
@@ -0,0 +1,31 @@
+echo "n=10"
+./runTest.sh 10.data 5000000 N
+echo "------------------------"
+echo "n=50"
+./runTest.sh 50.data 5000000 N
+echo "n=100"
+./runTest.sh 100.data 5000000 N
+echo "------------------------"
+echo "n=150"
+./runTest.sh 150.data 5000000 N
+echo "------------------------"
+echo "n=200"
+./runTest.sh 200.data 5000000 N
+echo "------------------------"
+echo "n=250"
+./runTest.sh 250.data 5000000 N
+echo "------------------------"
+echo "n=300"
+./runTest.sh 300.data 5000000 N
+echo "------------------------"
+echo "n=350"
+./runTest.sh 350.data 5000000 N
+echo "------------------------"
+echo "n=400"
+./runTest.sh 400.data 5000000 N
+echo "------------------------"
+echo "n=450"
+./runTest.sh 450.data 5000000 N
+echo "------------------------"
+echo "n=500"
+./runTest.sh 500.data 5000000 N
